d7a189e463b95a199f526e006640c872b52245b3
lux-02 <oys128950@gmail.com>
2022-04-15 14:11:18 +0000

    module

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const http = require('http');
 
-function onRequest(request, response) {
-    console.log('request received.');
-    response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.write('Hello Word');
-    response.end();
+function start(){
+    function onRequest(request, response) {
+        console.log('request received');
+        response.writeHead(200, {'Content-Type' : 'text/plain'});
+        response.write('Hello World');
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(8888);
+    
+    console.log('server has started');
 }
 
-http.createServer(onRequest).listen(8888);
+exports.start = start;
 
-console.log('Server has started');
